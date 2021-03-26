ea44081e6473e2fc3ca96b7bb0b474ae5fa67227
aoyo <https://github.com/2317689556/aoyo.git>
2021-03-26 11:31:55 +0800

    董莽

---
+++ src/main/java/com/bgs/PPRMApplication.java
@@ -0,0 +1,17 @@
+package com.bgs;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@MapperScan("com.bgs.mapper")
+@EnableAsync //开启异步
+public class PPRMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PPRMApplication.class, args);
+    }
+
+}
